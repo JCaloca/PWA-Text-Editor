c2dc80f4a72e3fcf0288ef079ad98a63676fe64d
JCaloca <jacobcaloca1@gmail.com>
2022-11-29 18:12:58 -0800

    Added logic for getting all content from indexedDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,8 +18,9 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log("PUT to the database");
-  const jate = await openDB("jate", 1);
-  const tx = jate.transaction("jate", "readwrite");
+
+  const jateDB = await openDB("jate", 1);
+  const tx = jateDB.transaction("jate", "readwrite");
   const store = tx.objectStore("jate");
   const request = store.put({ jate: content });
   const result = await request;
@@ -27,6 +28,14 @@ export const putDb = async (content) => {
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error("getDb not implemented");
+export const getDb = async () => {
+  console.log("GET all from the database");
+  const jateDB = await openDB("jate", 1);
+  const tx = jateDB.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.getAll();
+  const result = await request;
+  console.log("result.value", result);
+};
 
 initdb();
